c469734ed24c87845d5eb5abb30b2532424c354d
1zehaowang <zehaowang@163.com>
2017-08-15 14:05:02 +0800

    leetcode566

---
+++ 566/566.java
@@ -0,0 +1,13 @@
+public class Solution{
+    public int[][] matrixReshape(int[][] nums, int r, int c){
+        int[][] res = new int[r][c];
+        if(r*c != nums.length*nums[0].length){
+            return nums;
+        }
+        else{
+            for(int i = 0; i < r*c; i++){
+                res[i/c][i%c] = nums[i/nums[0].length][i%nums[0].length];
+            }
+        }
+}
+}
