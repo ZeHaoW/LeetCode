ba5219ec16b28a00b9e9e4b3d8b4dea2d2e20797
ZehaoW <wangzehao1994@gmail.com>
2017-08-16 20:49:26 +0800

    122

---
+++ 122/Solution.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public int maxProfit(int[] prices) {
+        int cnt = 0;
+      int profit = 0;
+        for(int i = 1; i < prices.length; i++){
+          if(prices[i] < prices[i-1]){
+            profit += prices[i-1] - prices[i-1-cnt];
+            cnt = 0;
+          }
+          else{
+            cnt++;
+          }
+          while(i == prices.length - 1) {
+        	  profit += prices[i] - prices[i-cnt];
+              break;
+          }
+        }
+        return profit;
+    }
+}
