d2451205c8fd67f200c9ed7b6c365a4d04037fe5
ZehaoW <zehaowang1994@gmail.com>
2017-09-02 16:45:49 +0800

    205nice

---
+++ 205nice/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+         int[] m = new int[512];
+        for(int i = 0; i < s.length(); i++){
+            if(m[s.charAt(i)] != m[t.charAt(i) + 256])
+                return false;
+            else
+                m[s.charAt(i)] = m[t.charAt(i) + 256] = i+1; 
+        }
+        return true;
+    }
+}
