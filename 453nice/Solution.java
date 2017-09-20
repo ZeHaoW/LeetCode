fc34b3ed3470660af28c9d0ccbe4936eb42f0447
wangzehao <zehaowang1994@gmail.com>
2017-09-20 14:42:05 +0800

    453nice

---
+++ 453nice/Solution.java
@@ -0,0 +1,11 @@
+class Solution{
+    public int minMoves(int[] nums) {
+     	int sum = 0;
+    	int min = Integer.MAX_VALUE;
+		for(int n : nums){
+			sum+=n;
+			if(n < min) min = n;
+		}
+		return sum-min*nums.length;    	
+	}
+}
--- 476/Solution.java
+++
@@ -1,7 +0,0 @@
-class Solution{
-	public int findComplement(int num){
-		num = ~num;
-		int mask = Integer.highestOneBit(num) - 1;
-		return num & mask;
-	}
-}
