809ac544aaa460949185eee783722409fe1f62ed
wangzehao <zehaowang1994@gmail.com>
2017-11-23 17:20:09 +0800

    191nice

---
+++ 191nice/Solution.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def hammingWeight(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        count =0
+        while n != 0: 
+            count += n&1
+            n>>1
+        return count
+
+if __name__ == '__main__':
+    print Solution().hammingWeight(3)
